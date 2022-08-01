ec6f9efb54f5889e499048520cf7d9543a692e7f
ruzwa <ruzwanali@gmail.com>
2022-08-02 02:42:12 +0500

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTHDOMAIN,
-  projectId: YOUR_PROJECTID,
-  storageBucket: YOUR_STORAGEBUCKET,
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-  appId: YOUR_APPID
+  apiKey: "AIzaSyA3qVT7CYh-OzULAW9bDcDIE0Q2groMTYQ",
+  authDomain: "todo-7d341.firebaseapp.com",
+  projectId: "todo-7d341",
+  storageBucket: "todo-7d341.appspot.com",
+  messagingSenderId: "906725758027",
+  appId: "1:906725758027:web:f9c05e09e43bed1ca169ce"
 };
 
 // Initialize Firebase
